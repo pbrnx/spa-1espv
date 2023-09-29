eb58ff6b10af825bb4a21cc98f87dd43e2a79077
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-28 21:11:21 -0300

    Modal exemplo

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,11 +1,19 @@
-
+import Modal from "../components/Modal";
+import {useState} from "react";
 export default function Produtos() {
   
   document.title = "PRODUTOS";
 
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <h1>Produtos</h1>
+
+        {open ? <Modal open={true} setOpen={setOpen}/> : ""}
+
+        <button onClick={()=> setOpen(true)}>OPEN-MODAL</button>
+
     </div>
   )
 }
