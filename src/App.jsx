da1cb758714bbbabddc419813e3468666d222a0e
alecarlojesus <profalexandre.jesus@fiap.com.br>
2023-10-19 20:02:54 -0300

    Estilizando o cabecalho com sass.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Cabecalho from "./components/Cabecalho";
-import Rodape from "./components/Rodape"
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape"
 import style from "./index.module.css";
 
 export default function App(){
--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "./Cabecalho.scss";
 
 export default function Cabecalho() {
   return (
@@ -10,13 +11,7 @@ export default function Cabecalho() {
            <Link to="/">Home</Link>
           </li>
           <li>
-          <Link to="/produtos">Produtos</Link>
-          </li>
-          <li>
-            <Link to="/editar/produtos/1">Editar Produto - 1</Link>
-          </li>
-          <li>
-            <Link to="/editar/produtos/2">Editar Produto - 2</Link>
+            <Link to="/produtos">Produtos</Link>
           </li>
         </ul>
       </header>
