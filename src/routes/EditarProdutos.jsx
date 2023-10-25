f3b5e711119d8c242f349a53188471c353612c7e
pbrnx <128100284+pbrnx@users.noreply.github.com>
2023-10-25 20:56:43 -0300

    Add files via upload

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,59 +1,71 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import data from '../dados.json';
 
 export default function EditarProdutos() {
   
-    const {id} = useParams();
+  const { id } = useParams();
+  const productFromData = data.produtos.find(p => p.id === Number(id));
 
-    const[produto, setProduto] = useState({
-      id:id,
-      nome:"",
-      desc:"",
-      valor:""
-    });
+  const [produto, setProduto] = useState(productFromData || {
+    id: "",
+    nome: "",
+    desc: "",
+    valor: ""
+  });
 
-    useEffect(() => {
-      
-      //http://localhost:5000/produtos/2
-  
-      //Realizando uma requisição para a API-JSON utilizando
-      // o id como parâmetro, para receber um único produto/objeto
-      //Para edição.
-      fetch(`http://localhost:5000/produtos/${id}`)
-      .then((response)=> response.json())
-      .then((response)=> setProduto(response))
-      .catch((error)=> console.error(error));
-    }, [id])
-    
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProduto(prevState => ({ ...prevState, [name]: value }));
+  };
 
+  // You'd normally make an API call to save the edits
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Produto atualizado.');
+  };
 
   return (
     <div>
       <h1>Editar Produtos</h1>
 
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <fieldset>
             <legend>Produto Selecionado</legend>
             <div>
-              <label htmlFor="">Nome</label>
-              <input type="text" name="nome" value={produto.nome} onChange={(e)=>setProduto(e.target.value)}/>
+              <label htmlFor="nome">Nome</label>
+              <input 
+                type="text" 
+                name="nome" 
+                value={produto.nome} 
+                onChange={handleInputChange}
+              />
             </div>
             <div>
-              <label htmlFor="">Desc</label>
-              <input type="text" name="desc" value={produto.desc} onChange={(e)=>setProduto(e.target.value)}/>
+              <label htmlFor="desc">Desc</label>
+              <input 
+                type="text" 
+                name="desc" 
+                value={produto.desc} 
+                onChange={handleInputChange}
+              />
             </div>
             <div>
-              <label htmlFor="">Valor</label>
-              <input type="number" name="valor" value={produto.valor} onChange={(e)=>setProduto(e.target.value)}/>
+              <label htmlFor="valor">Valor</label>
+              <input 
+                type="number" 
+                name="valor" 
+                value={produto.valor} 
+                onChange={handleInputChange}
+              />
             </div>
             <div>
-              <button>EDITAR</button>
+              <button type="submit">EDITAR</button>
             </div>
           </fieldset>
         </form>
       </div>
-
     </div>
   )
 }
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,70 +1,107 @@
-import React, { useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import data from '../dados.json';
+import "./Produtos.scss";
 
 export default function Produtos() {
   document.title = "PRODUTOS";
-
-  const [valorData, setValorData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  
+  const [listaProdutosAPI, setListaProdutosAPI] = useState([]);
 
   useEffect(() => {
-    // Fetch valor data when the component mounts
-    fetch("http://localhost:5174/produtos", {
-      method: "GET",
-      headers: {
-        "Accept": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((response) => {
-        setValorData(response.map((item) => item.valor));
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
+    setListaProdutosAPI(data.produtos);
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const addProduct = (product) => {
+    setListaProdutosAPI([...listaProdutosAPI, { ...product, id: Date.now() }]);
+  };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const deleteProduct = (id) => {
+    const newProducts = listaProdutosAPI.filter(product => product.id !== id);
+    setListaProdutosAPI(newProducts);
+  };
 
   return (
     <div>
       <h1>Produtos</h1>
+      <AddProduct onAdd={addProduct} />
 
       <div className="tblProdutos">
         <table>
           <thead>
-            <tr>
-              <th>Valor</th>
+            <tr>            
+              <th>NOME</th>
+              <th>DESCRIÇÃO</th>
+              <th>VALOR</th>
+              <th>EDITAR/EXCLUIR</th>
             </tr>
           </thead>
           <tbody>
-            {valorData.map((valor, indice) => (
-              <tr key={indice}>
-                <td>{valor}</td>
+            {listaProdutosAPI.map((item) => (
+              <tr key={item.id}>               
+                <td>{item.nome}</td>
+                <td>{item.desc}</td>
+                <td>{item.valor}</td>
+                <td>
+                  <Link to={`/editar/produtos/${item.id}`}>Editar</Link>
+                  <button onClick={() => deleteProduct(item.id)}>Delete</button>
+                </td>
               </tr>
             ))}
           </tbody>
           <tfoot>
             <tr>
-              <td>PRODUTOS / Qtd = {valorData.length}</td>
+              <td colSpan={4}>PRODUTOS / Qtd = {listaProdutosAPI.length}</td>
             </tr>
           </tfoot>
         </table>
       </div>
+    </div>
+  );
+}
+
+function AddProduct({ onAdd }) {
+  const [product, setProduct] = useState({
+    nome: "",
+    desc: "",
+    valor: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (product.nome && product.desc && product.valor) {
+      onAdd(product);
+      setProduct({
+        nome: "",
+        desc: "",
+        valor: ""
+      });
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Nome" 
+          value={product.nome}
+          onChange={(e) => setProduct({ ...product, nome: e.target.value })}
+        />
+        <input 
+          type="text" 
+          placeholder="Desc" 
+          value={product.desc}
+          onChange={(e) => setProduct({ ...product, desc: e.target.value })}
+        />
+        <input 
+          type="number" 
+          placeholder="Valor" 
+          value={product.valor}
+          onChange={(e) => setProduct({ ...product, valor: e.target.value })}
+        />
+        <button type="submit">Adicionar Produto</button>
+      </form>
     </div>
   );
 }
